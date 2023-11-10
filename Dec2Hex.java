ba770c9d4151e7f4355b9c431a38a40b3b594a49
vrusso300 <vrusso300@caledonian.ac.uk>
2023-11-10 14:42:53 +0000

    Testing Jenkins Automation

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -4,7 +4,7 @@ class Dec2Hex
 {
 
     public static int Arg1;
-
+    //This is a test for git push to remote pos.
     public static void main(String args[])
     {
         Arg1 = Integer.parseInt(args[0]);
@@ -15,7 +15,7 @@ class Dec2Hex
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
         while(num != 0)
-        {
+        {   
             rem=num%16;
             hexadecimal= ch[rem] + hexadecimal;
             num= num/16;

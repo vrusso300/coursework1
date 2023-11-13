8a7d81c835619fdfa43d9f41bf26e9c4256d453e
vrusso300 <vrusso300@caledonian.ac.uk>
2023-11-13 15:59:17 +0000

    Add stringbuilder and fix while loop to save memory performance

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -41,14 +41,24 @@ class Dec2Hex
 
                 System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
+
+                //Initilise a new instance of the StringBuilder method as reference 'bld'
+                StringBuilder bld =  new StringBuilder();
+
                 while(num != 0)
                 {   
                     rem=num%16;
-                    hexadecimal= ch[rem] + hexadecimal;
+
+                    //Append string to be the character of rem + hexadecimal 
+                    bld.append(ch[rem] + hexadecimal);
+
                     num= num/16;
                 }
 
-                System.out.println("Hexadecimal representation is: " + hexadecimal);
+                //Create new string to be = to the characters of the stringbuilder 
+                String str = bld.toString();
+
+                System.out.println("Hexadecimal representation is: " + str);
             } 
             //Error is caught, convert exception to string and print it
             catch (Exception e)
@@ -60,7 +70,6 @@ class Dec2Hex
 
     }
 }
-
 
 
 

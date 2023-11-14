1f04a094147b455aebad7abe991d805bdbdfb7cb
vrusso300 <vrusso300@caledonian.ac.uk>
2023-11-14 17:12:59 +0000

    Update Java file. Added method.

--- com/cw1/Dec2Hex.java
+++ com/cw1/Dec2Hex.java
@@ -1,80 +1,78 @@
 package com.cw1;
-
-class Dec2Hex
-{
+class Dec2Hex {
   
-    //Add private access modifier to declaration so that the value cannot be accessed outwith the class
-    //Following encapsulation principles
+    // Add private access modifier to declaration so that the value cannot be accessed outwith the class
+    // Following encapsulation principles
     private static int arg1 = 0;
 
-    //Declare getters and setters
-    public static int getarg1() {
+    // Declare getter and setter
+    public static int getArg1() {
         return arg1;
     }
 
-    public static void setarg1(int value) {
+    public static void setArg1(int value) {
         arg1 = value;
     } 
 
-    public static void main(String[] args)
-    {
-        
-        //Checking if String array args is empty, if so, print error statement
-        if(args.length == 0)
-        {
-            System.out.println("Error: Please enter a denary value.");
+    public static String convertToHex(int decimalNo) {
+
+        // Initialize char collection with hexadecimal values
+        char[] ch = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem;
+        int num = decimalNo;
+
+        // Initialize a new instance of the StringBuilder method as reference 'bld'
+        StringBuilder bld = new StringBuilder();
+
+        // While loop to convert into hexadecimal
+        while (num != 0) {   
+            rem = num % 16;
+
+            // Append string to be the character of rem + hexadecimal 
+            bld.append(ch[rem]);
+            num = num / 16;
         }
-        //Else, run as normal
-        else if(args.length ==1)
-        {   
-            //Try normal conversion, if error is detected - print error in catch statement
-            try 
-            {
-                //Call set method to set the parsed input value
-                setarg1(Integer.parseInt(args[0]));
-                char[] ch={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-                int rem;
-                int num;
-                //Get the arg1 value from the Arg1 method
-                num = getarg1();
-                String hexadecimal="";
 
+        // Create new string to be = to the characters of the StringBuilder 
+        String str = bld.reverse().toString();
 
+        // Return hexadecimal value
+        return str;
+    }
+
+    public static void main(String[] args) {
+        
+        // Checking if String array args is empty, if so, print an error statement
+        if (args.length == 0) {
+            System.out.println("Error: Please enter a denary value.");
+        } 
+        // Else, run as normal
+        else if (args.length == 1) {   
+            // Try normal conversion, if an error is detected - print error in the catch statement
+            try {
+                // Call set method to set the parsed input value
+                setArg1(Integer.parseInt(args[0]));
+                
+                // Get the arg1 value from the getArg1 method
+                int num = getArg1();
+
+                // Local variable hexadecimal = to the returned number from convertToHex method
+                String hexadecimal = convertToHex(num);
+
+                // Print the decimal
                 System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-
-                //Initilise a new instance of the StringBuilder method as reference 'bld'
-                StringBuilder bld =  new StringBuilder();
-
-                while(num != 0)
-                {   
-                    rem=num%16;
-
-                    //Append string to be the character of rem + hexadecimal 
-                    bld.append(ch[rem] + hexadecimal);
-
-                    num= num/16;
-                }
-
-                //Create new string to be = to the characters of the stringbuilder 
-                String str = bld.toString();
-
-                System.out.println("Hexadecimal representation is: " + str);
+                // Print the hexadecimal
+                System.out.println("Hexadecimal representation is: " + hexadecimal);
             } 
-            //Error is caught, convert exception to string and print it
-            catch (Exception e)
-            {
+            // Error is caught, convert exception to string and print it
+            catch (Exception e) {
                 System.out.println(e.toString());
             }
-            
         }
-        else
-        {
-            System.out.println("Must only be 1 value.");
+        // Else, print an error    
+        else {
+            System.out.println("Error: must only be 1 value.");
         }
-
     }
-   
 }
-
-
